11464b1af4b1facd5e240c4ef9832bcd1c334955
pvan52969-creator <pvan52969@gmail.com>
2026-09-20 10:46:00 +0800

    0920开课

--- prototype/app.js
+++ prototype/app.js
@@ -68131,7 +68131,7 @@ function enterScheduleModule() {
   // setActiveModule('schedule');
   // goPage('schedule-workflow');
   // 改：跳转至外部排课原型 DEMO
-  const url = 'https://roundworm-scrawny-struggle.ngrok-free.dev/';
+  const url = 'https://pvan52969-creator.github.io/PYFA/feature/pkgl/';
   window.open(url, '_blank', 'noopener,noreferrer');
 }
 
